4384f3df02566aa34e72fb3f77edddff9b0f042b
HZH <capric98@outlook.com>
2019-12-10 06:43:36 +0800

    give up

--- rss/ticker.go
+++ rss/ticker.go
@@ -2,12 +2,11 @@ package rss
 
 import (
 	"context"
-	"io"
 	"log"
 	"net/http"
-	"runtime"
 	"time"
 
+	"github.com/capric98/t-rss/myfeed"
 	"github.com/capric98/t-rss/torrents"
 )
 
@@ -74,24 +73,24 @@ func (t *ticker) fetch(req *http.Request, ch chan []torrents.Individ) {
 	}
 	data = data[:0]
 	_, rerr = resp.Body.Read(chunk)
-	for rerr != io.EOF {
+	for rerr == nil {
 		data = append(data, chunk[0])
 		_, rerr = resp.Body.Read(chunk)
 	}
 	resp.Body.Close()
-	//_, _ = myfeed.Parse(data)
+	rssFeed, _ := myfeed.Parse(data)
 
-	// for k := range rssFeed.Items {
-	// 	if rssFeed.Items[k].Enclosure.Url == "" {
-	// 		rssFeed.Items[k].Enclosure.Url = rssFeed.Items[k].Link
-	// 	}
-	// 	if rssFeed.Items[k].GUID.Value == "" {
-	// 		rssFeed.Items[k].GUID.Value = myfeed.NameRegularize(rssFeed.Items[k].Title)
-	// 	}
-	// 	rssFeed.Items[k].GUID.Value = myfeed.NameRegularize(rssFeed.Items[k].GUID.Value)
-	// }
+	for k := range rssFeed.Items {
+		if rssFeed.Items[k].Enclosure.Url == "" {
+			rssFeed.Items[k].Enclosure.Url = rssFeed.Items[k].Link
+		}
+		if rssFeed.Items[k].GUID.Value == "" {
+			rssFeed.Items[k].GUID.Value = myfeed.NameRegularize(rssFeed.Items[k].Title)
+		}
+		rssFeed.Items[k].GUID.Value = myfeed.NameRegularize(rssFeed.Items[k].GUID.Value)
+	}
 
 	log.Printf("%s fetched in %7.2fms.", t.name, time.Since(startT).Seconds()*1000.0)
-	//ch <- rssFeed.Items
-	runtime.GC()
+	ch <- rssFeed.Items
+	//runtime.GC()
 }
