ea10e16019a578cc611c61ae1fb4cf2a65316bdf
HZH <capric98@outlook.com>
2020-04-13 04:04:26 +0800

    tmp

--- t-rss.go
+++ t-rss.go
@@ -3,6 +3,7 @@ package trss
 import (
 	"os"
 
+	"github.com/capric98/t-rss/setting"
 	"github.com/mattn/go-colorable"
 	"github.com/sirupsen/logrus"
 )
@@ -10,11 +11,34 @@ import (
 // WithConfigFile starts program using a config file.
 func WithConfigFile(filename string, learn bool) {
 	backgroundLogger := logrus.New()
-	backgroundLogger.SetOutput(colorable.NewColorableStdout())
+	formatter := &logrus.TextFormatter{
+		ForceColors:     false,
+		FullTimestamp:   true,
+		PadLevelText:    true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "@time",
+			logrus.FieldKeyLevel: "level",
+			logrus.FieldKeyMsg:   "@msg",
+		},
+	}
+	backgroundLogger.SetFormatter(formatter)
 
 	fr, e := os.Open(filename)
 	if e != nil {
 		backgroundLogger.Fatal("open config file:", e)
 	}
-	defer fr.Close()
+	config, e := setting.Parse(fr)
+	fr.Close()
+	if e != nil {
+		backgroundLogger.Fatal("parse config file:", e)
+	}
+	if config.Global.LogConfig.Save == "" {
+		formatter.ForceColors = true
+		backgroundLogger.WithField("testField", "!!!").Info("Output log to stderr...")
+		backgroundLogger.SetOutput(colorable.NewColorableStderr())
+	}
+	backgroundLogger.SetLevel(logrus.DebugLevel)
+	backgroundLogger.Debug(config)
+	backgroundLogger.Info("hahahaha")
 }
