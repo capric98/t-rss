7170384f8d66e96625c7e071de03c5bb6c175a59
HZH <capric98@outlook.com>
2020-04-26 00:17:08 +0800

    bump version to v0.6.7-beta

--- version.go
+++ version.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	version = "v0.6.6-beta"
+	version = "v0.6.7-beta"
 	intro   = fmt.Sprintf("t-rss %v %v/%v (%v build)\n", version, runtime.GOOS, runtime.GOARCH, runtime.Version())
 )
 
